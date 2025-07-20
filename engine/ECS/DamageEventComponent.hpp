605ee9fdf18c0ff7a6e5b3ff87fae0f850b492ba
ArcticNoise <mepkypuo@gmail.com>
2025-07-20 23:31:57 +0800

    fix: silence struct padding warnings on msvc

--- engine/ECS/DamageEventComponent.hpp
+++ engine/ECS/DamageEventComponent.hpp
@@ -3,10 +3,17 @@
 namespace x2d
 {
 
+#ifdef _MSC_VER
+#    pragma warning(push)
+#    pragma warning(disable:4324)
+#endif
 struct alignas(16) DamageEventComponent
 {
     int amount;
 };
+#ifdef _MSC_VER
+#    pragma warning(pop)
+#endif
 static_assert(sizeof(DamageEventComponent) <= 64);
 
 } // namespace x2d
--- engine/ECS/VelocityComponent.hpp
+++ engine/ECS/VelocityComponent.hpp
@@ -3,11 +3,18 @@
 namespace x2d
 {
 
+#ifdef _MSC_VER
+#    pragma warning(push)
+#    pragma warning(disable:4324)
+#endif
 struct alignas(16) VelocityComponent
 {
     float vx;
     float vy;
 };
+#ifdef _MSC_VER
+#    pragma warning(pop)
+#endif
 static_assert(sizeof(VelocityComponent) <= 64);
 
 } // namespace x2d
