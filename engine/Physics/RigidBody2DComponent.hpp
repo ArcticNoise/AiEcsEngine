349ae47a6bb702ad572e6e06ee3a8e0bccae21b0
ArcticNoise <mepkypuo@gmail.com>
2025-07-21 01:28:57 +0800

    fix: make RigidBody2DComponent trivial

--- engine/Physics/RigidBody2DComponent.hpp
+++ engine/Physics/RigidBody2DComponent.hpp
@@ -5,8 +5,8 @@
 namespace x2d {
 
 struct alignas(16) RigidBody2DComponent {
-    Vector2 velocity{};
-    bool isStatic{false};
+    Vector2 velocity;
+    bool isStatic;
 };
 static_assert(sizeof(RigidBody2DComponent) <= 64);
 
