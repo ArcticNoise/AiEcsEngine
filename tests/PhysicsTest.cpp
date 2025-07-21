d379bfa9b1974b79c3fbd02b1456125fb4a5f43e
ArcticNoise <mepkypuo@gmail.com>
2025-07-21 14:49:41 +0800

    fix: remove using namespace from tests

--- tests/PhysicsTest.cpp
+++ tests/PhysicsTest.cpp
@@ -7,46 +7,44 @@
 #include "Physics/PhysicsSystem.hpp"
 #include "Physics/RigidBody2DComponent.hpp"
 
-using namespace x2d;
-
 TEST_CASE("EventQueue enqueue/dequeue", "[physics]") {
-    EventQueue<CollisionEvent, 4> q;
+    x2d::EventQueue<x2d::CollisionEvent, 4> q;
     REQUIRE(q.IsEmpty());
     q.Enqueue({1, 2});
-    CollisionEvent ev{};
+    x2d::CollisionEvent ev{};
     REQUIRE(q.Dequeue(ev));
     REQUIRE(ev.a == 1);
     REQUIRE(ev.b == 2);
 }
 
 TEST_CASE("PhysicsSystem detects collision", "[physics]") {
-    World world;
-    Signature sig;
-    sig.set(ComponentTypeId<TransformComponent>());
-    sig.set(ComponentTypeId<RigidBody2DComponent>());
-    sig.set(ComponentTypeId<ColliderAABBComponent>());
+    x2d::World world;
+    x2d::Signature sig;
+    sig.set(x2d::ComponentTypeId<x2d::TransformComponent>());
+    sig.set(x2d::ComponentTypeId<x2d::RigidBody2DComponent>());
+    sig.set(x2d::ComponentTypeId<x2d::ColliderAABBComponent>());
 
-    EventQueue<CollisionEvent> collisions;
-    EventQueue<TriggerEvent> triggers;
-    world.GetSystemManager().RegisterSystem<PhysicsSystem>(ESystemPhase::eSystemPhase_FixedUpdate,
+    x2d::EventQueue<x2d::CollisionEvent> collisions;
+    x2d::EventQueue<x2d::TriggerEvent> triggers;
+    world.GetSystemManager().RegisterSystem<x2d::PhysicsSystem>(x2d::ESystemPhase::eSystemPhase_FixedUpdate,
                                                            sig, collisions, triggers);
 
-    Entity a = world.CreateEntity();
-    world.AddComponent<TransformComponent>(a, {0.0f, 0.0f, 0.0f, 1.0f});
-    world.AddComponent<RigidBody2DComponent>(a, {});
-    world.AddComponent<ColliderAABBComponent>(a, {0.5f, 0.5f, false});
+    x2d::Entity a = world.CreateEntity();
+    world.AddComponent<x2d::TransformComponent>(a, {0.0f, 0.0f, 0.0f, 1.0f});
+    world.AddComponent<x2d::RigidBody2DComponent>(a, {});
+    world.AddComponent<x2d::ColliderAABBComponent>(a, {0.5f, 0.5f, false});
 
-    Entity b = world.CreateEntity();
-    world.AddComponent<TransformComponent>(b, {0.4f, 0.0f, 0.0f, 1.0f});
-    world.AddComponent<RigidBody2DComponent>(b, {});
-    world.AddComponent<ColliderAABBComponent>(b, {0.5f, 0.5f, false});
+    x2d::Entity b = world.CreateEntity();
+    world.AddComponent<x2d::TransformComponent>(b, {0.4f, 0.0f, 0.0f, 1.0f});
+    world.AddComponent<x2d::RigidBody2DComponent>(b, {});
+    world.AddComponent<x2d::ColliderAABBComponent>(b, {0.5f, 0.5f, false});
 
     auto &sm = world.GetSystemManager();
     sm.BuildViews(world);
-    sm.ForEachSystemInPhase(ESystemPhase::eSystemPhase_FixedUpdate,
-                            [](ISystem &sys, const View &view) { sys.Update(view); });
+    sm.ForEachSystemInPhase(x2d::ESystemPhase::eSystemPhase_FixedUpdate,
+                            [](x2d::ISystem &sys, const x2d::View &view) { sys.Update(view); });
 
-    CollisionEvent evt{};
+    x2d::CollisionEvent evt{};
     REQUIRE(collisions.Dequeue(evt));
     REQUIRE(((evt.a == a && evt.b == b) || (evt.a == b && evt.b == a)));
 }
